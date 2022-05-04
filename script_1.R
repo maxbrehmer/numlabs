b90a9d8c6ec951a373d4bef836d213db28598060
Max Brehmer <maxbrehmer@Brehmers-Macbook.local>
2022-05-04 05:11:53 +0200

    Transformed data to wide format

--- script_1.R
+++ script_1.R
@@ -3,3 +3,14 @@ readh <- function(dataset, N) {
   
   return(head(data, N))
 }
+
+per_capita <- function(var, pop) {
+  action <- mutate(new_var = var/pop)
+  return(action)
+}
+
+add_loop = function(list, pop) {
+  for (i in list) {
+    per_capita(i, pop)
+  }
+}
